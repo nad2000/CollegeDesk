cd7cc27c573fbd6e5bae532c5ed75087636245cd
Rad Cirskis <nad2000@gmail.com>
2018-11-15 23:16:00 +1300

    missing files

---
+++ model/xlsx/shared_strings.go
@@ -0,0 +1,16 @@
+package xlsx; import "encoding/xml"
+// Sst was generated 2018-11-15 20:05:32 by rcir178 on rcir178.
+type Sst struct {
+	XMLName     xml.Name `xml:"sst"`
+	Text        string   `xml:",chardata"`
+	Xmlns       string   `xml:"xmlns,attr"`
+	Count       string   `xml:"count,attr"`
+	UniqueCount string   `xml:"uniqueCount,attr"`
+	Si          []struct {
+		Text string `xml:",chardata"`
+		T    struct {
+			Text string `xml:",chardata"` // Month, Salesman, Region, ...
+		} `xml:"t"`
+	} `xml:"si"`
+} 
+
